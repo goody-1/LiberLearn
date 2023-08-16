adf22bb14f15a4c647bb7a09040c2302fd88fdc0
Atunde Goodness <atundeg@gmail.com>
2023-08-16 20:14:08 +0100

    add detail

--- src/resources/landing/mentors.js
+++ src/resources/landing/mentors.js
@@ -1,14 +1,15 @@
 import './mentors.css';
 
-function Mentors(props) { 
-    return (
-      <div className="mentors">
-            <img src={props.image} alt={ props.mentor } />
-        <h2>{props.mentor}</h2>
-        <h3>{props.title}</h3>
-        <p>{props.information}</p>
-      </div>
-    );
+function Mentors(props) {
+  return (
+    <div className="mentors">
+      <img src={props.image} alt={props.mentor} />
+      <h2>{props.mentor}</h2>
+      <p>Instructor for</p>
+      <h3>{props.title}</h3>
+      <p>{props.information}</p>
+    </div>
+  );
 }
 
 export default Mentors;
